d5f85049416102c14b0d3d9e2ac74e76c5c01b20
Steph <swils35@acd.ccac.edu>
2019-09-11 19:25:52 -0400

    challenges

---
+++ challenge2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 11 19:21:43 2019
+
+@author: StephanieWilson
+"""
+
+bang = ["K", "A", "B", "O", "O", "M"]
+
+for i in range(len(bang)):
+    print (bang[i], end="")
+    print (bang[i], end="")
+    print (bang[i], end="")
---
+++ challenge3.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 11 18:50:14 2019
+
+@author: StephanieWilson
+"""
+
+# slice the string so it only outputs every other character
+
+string = "askaliceithinkshe'llknow"
+
+list = string.split()
+
+for i in range(1, len(list), 2):
+    print(list[i])
+
