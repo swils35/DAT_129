4b20b9bda92683e51c7791dae3e583e1f3303d9c
Steph <swils35@acd.ccac.edu>
2019-09-11 19:21:29 -0400

    challenge 1

---
+++ challenge1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 11 19:18:58 2019
+
+@author: StephanieWilson
+"""
+
+# count to 100, skipping all odd numbers
+# print all in a row
+
+for i in range(0, 101, 2):
+    print(i, end=" ")
